113b43b5a9033c621dc9617e9ae244b4d5b733d1
Sherita Sherlene <sheritsh@et-g5.kzn.21-school.ru>
2023-04-22 00:05:36 +0300

    feat: unit_tests mock created

---
+++ src/tests/unit_tests.c
@@ -0,0 +1,3 @@
+#include "unit_tests.h"
+
+int main(void) { return 0; }
---
+++ src/tests/unit_tests.h
@@ -0,0 +1,9 @@
+/* created by sheritsh // Oleg Polovinko ※ School 21, Kzn */
+#ifndef UNIT_TESTS_H
+#define UNIT_TESTS_H
+
+#include <check.h>
+
+#include "../s21_matrix.h"
+
+#endif  // UNIT_TESTS_H
