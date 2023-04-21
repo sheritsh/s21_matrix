0c9d1fd0d577e6726204dc04ff639f90c9d6a91a
Sherita Sherlene <sheritsh@et-g5.kzn.21-school.ru>
2023-04-22 01:55:38 +0300

    test: testing matrix allocate methods

---
+++ src/test_two_alloc_matrix.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define rows 5
+#define columns 5
+
+void initialize_matrix(double **A);
+void print_matrix(double **A);
+
+int main(void) {
+  double **A = (double **)malloc(rows * sizeof(double *));
+  double *vals = (double *)malloc(rows * columns * sizeof(double));
+  for (int i = 0; i < columns; i++) {
+    A[i] = vals + rows * i;
+  }
+  initialize_matrix(A);
+  print_matrix(A);
+  printf("-----------------\n");
+  printf("[%.f]", A[1][1]);
+  free(A);
+  free(vals);
+  A = NULL;
+  vals = NULL;
+  return 0;
+}
+
+void initialize_matrix(double **A) {
+  int counter = 1;
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < columns; j++) {
+      A[i][j] = counter;
+      counter++;
+    }
+  }
+}
+
+void print_matrix(double **A) {
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < columns; j++) {
+      printf("%.f\t", A[i][j]);
+    }
+    printf("\n");
+  }
+}
