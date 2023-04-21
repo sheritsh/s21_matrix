c5515beb5c81693a2d84ded674e1be6996f29baf
Sherita Sherlene <sheritsh@et-g5.kzn.21-school.ru>
2023-04-21 21:36:23 +0300

    feat: create module structure and mock functions

---
+++ src/modules/s21_determinant.c
@@ -0,0 +1,3 @@
+#include "../s21_matrix.h"
+
+int s21_determinant(matrix_t *A, double *result) { return 0; }
---
+++ src/modules/s21_mult_number.c
@@ -0,0 +1,3 @@
+#include "../s21_matrix.h"
+
+int s21_mult_number(matrix_t *A, double number, matrix_t *result) { return 0; }
